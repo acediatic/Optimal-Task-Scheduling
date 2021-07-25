e44bca604ddd8c4401d4e89a26c9b6f4a7238e5b
Remus Courtenay <rcou199@aucklanduni.ac.nz>
2021-07-25 13:20:13 +1200

    First attempt at TaskPrerequisiteState object

---
+++ src/main/java/softeng/project1/graphObjects/TaskPrerequisiteState.java
@@ -0,0 +1,48 @@
+package softeng.project1.graphObjects;
+
+public class TaskPrerequisiteState implements TaskPrerequisite{
+
+    private final int communicationCost;
+    private final TaskNode parentTask;
+    private final TaskNode childTask;
+
+    private final boolean isFulfilled;
+
+    public TaskPrerequisiteState(TaskPrerequisite previousState, TaskNode newTaskNode) {
+
+        this.communicationCost = previousState.getCommunicationCost();
+
+        // This could be optimised
+        if (previousState.getParentTask().getTaskName().equals(newTaskNode.getTaskName())) {
+            this.parentTask = newTaskNode;
+            this.childTask = previousState.getChildTask();
+            this.isFulfilled = true;
+        } else if (previousState.getChildTask().getTaskName().equals(newTaskNode.getTaskName())) {
+            this.parentTask = previousState.getParentTask();
+            this.childTask = newTaskNode;
+            this.isFulfilled = false;
+        } else {
+            throw new RuntimeException(""); // TODO...
+        }
+    }
+
+    @Override
+    public int getCommunicationCost() {
+        return this.communicationCost;
+    }
+
+    @Override
+    public TaskNode getParentTask() {
+        return this.parentTask;
+    }
+
+    @Override
+    public TaskNode getChildTask() {
+        return this.childTask;
+    }
+
+    public boolean isFulfilled() {
+        return this.isFulfilled;
+    }
+
+}
