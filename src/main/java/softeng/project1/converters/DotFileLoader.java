c5c24af2d63762b59908a62aae2e3c59f892073b
osamakashif <osamakashif1@hotmail.com>
2021-08-06 09:57:28 +1200

    feat - Setting up DOT Loader
    Setting up DOT Loader using GraphStream.

--- src/main/java/softeng/project1/converters/DotFileLoader.java
+++ src/main/java/softeng/project1/converters/DotFileLoader.java
@@ -1,8 +1,31 @@
 package softeng.project1.converters;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.DefaultGraph;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceFactory;
+
+import java.io.IOException;
+
 /**
- * TODO...
+ * DotFileLoader uses GraphStream to read the DOT file and adds it to it's graph representation.
  */
 public class DotFileLoader {
+    private Graph graph = new DefaultGraph("graph");
+    private FileSource fileSource;
+    //FileSource fs = FileSourceFactory.sourceFor("path/to/my/file");
+
+    public DotFileLoader() {
+        //
+    }
+    public DotFileLoader(String path) {
+        try {
+            fileSource = FileSourceFactory.sourceFor(path);
+            fileSource.addSink(graph);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException thrown for file source path");
+        }
+    }
 
 }
