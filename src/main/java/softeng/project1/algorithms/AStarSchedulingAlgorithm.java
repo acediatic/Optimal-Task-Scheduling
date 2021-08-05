9dd3c9f585e258200a816a4dbe9696b845211ce7
Remus Courtenay <rcou199@UoA.auckland.ac.nz>
2021-08-06 11:47:26 +1200

    [feat] Skeleton class for AStarSchedulingAlgorithm

---
+++ src/main/java/softeng/project1/algorithms/AStarSchedulingAlgorithm.java
@@ -0,0 +1,33 @@
+package softeng.project1.algorithms;
+
+import softeng.project1.algorithms.heuristics.HeuristicManager;
+import softeng.project1.graph.Schedule;
+import softeng.project1.graph.ScheduleState;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class AStarSchedulingAlgorithm implements SchedulingAlgorithm{
+
+    private final Schedule originalSchedule;
+    private final HeuristicManager heuristicManager;
+    private final Map<Long, ScheduleState> closedSchedules;
+
+    public AStarSchedulingAlgorithm(Schedule originalSchedule) {
+        this.originalSchedule = originalSchedule;
+        this.heuristicManager = new PriorityQueueHeuristicManager();
+        this.closedSchedules = new ConcurrentHashMap<>();
+    }
+
+
+
+
+
+
+
+
+
+
+}
