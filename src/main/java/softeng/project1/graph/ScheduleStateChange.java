b8334e4782c5dcfa24fca23e8266886255164cf4
Remus Courtenay <rcou199@aucklanduni.ac.nz>
2021-08-06 12:29:53 +1200

    [feat] skeleton for ScheduleStateChange class

--- src/main/java/softeng/project1/graph/ScheduleStateChange.java
+++ src/main/java/softeng/project1/graph/ScheduleStateChange.java
@@ -2,6 +2,10 @@ package softeng.project1.graph;
 
 import softeng.project1.graph.tasks.TaskNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ScheduleStateChange {
     private ScheduleStateChange previousChange;
     private TaskNode insertedTaskNode;
@@ -13,5 +17,18 @@ public class ScheduleStateChange {
         this.insertedTaskNode = insertedTaskNode;
         this.processorID = processorID;
         this.insertionTime = insertionTime;
+    }
+
+    public List<int[]> rebuildSolutionPath() {
+        int[] pathData = new int[]{processorID, insertionTime, insertedTaskNode.getTaskID()};
+
+        // First step
+        if (previousChange == null) {
+            return Arrays.asList(new int[][]{pathData});
+        } else {
+            List<int[]> returnList = previousChange.rebuildSolutionPath();
+            returnList.add(pathData);
+            return returnList;
+        }
     }
 }
