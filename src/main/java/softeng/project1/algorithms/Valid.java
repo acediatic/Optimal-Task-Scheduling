6dbe7b8d178ecd37cc98e37db660f7626b3c61c9
osamakashif <osamakashif1@hotmail.com>
2021-08-07 15:36:57 +1200

    feat - Valid scheduler restructured; fix - Valid scheduler put into algorithms package
    Valid now implements SchedulingAlgorithm, and is in the algorithms package now.
    Basic structure of Valid setup.

---
+++ src/main/java/softeng/project1/algorithms/Valid.java
@@ -0,0 +1,103 @@
+package softeng.project1.algorithms;
+
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import softeng.project1.graph.OriginalScheduleState;
+import softeng.project1.graph.processors.Processors;
+import softeng.project1.graph.processors.processor.Processor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Valid implements SchedulingAlgorithm {
+    private Graph graph; // Graph to process
+    List<Task> tasksInTopology; // Need to be implemented from graph data
+    private List<ValidProcessor> processors = new ArrayList<ValidProcessor>();
+    private static int defaultNumberOfProcessors = 1;  //default number of processors to begin with
+    private int numberOfProcessors;
+//    private List<Processors> processors = new ArrayList<Processors>();
+
+    public Valid(Graph read) {
+        this(read, defaultNumberOfProcessors);
+    }
+
+    public Valid(Graph read, int numberOfProcessors) {
+        this.graph = read;
+        this.numberOfProcessors = numberOfProcessors;
+        for (int i = 0; i<this.numberOfProcessors; i++) {
+            ValidProcessor processor = new ValidProcessor(i);
+            processors.add(processor);
+        }
+    }
+
+    private void compareAndAdd (Task task) {
+        int min = 0;
+        int lowestTime = processors.get(min).getOngoingTime();
+        for (ValidProcessor processor: processors) {
+            if (processor.getOngoingTime() < lowestTime) {
+                lowestTime = processor.getOngoingTime();
+                min = processor.getId();
+            }
+        }
+        processors.get(min).addTask(task);
+    }
+
+    public void schedule() {
+        for (Task task: tasksInTopology) {
+            compareAndAdd(task);
+        }
+    }
+
+    private class ValidProcessor {
+        private int id;
+        private List<Task> tasks = new ArrayList<Task>();
+        private int ongoingTime = 0;
+
+        protected ValidProcessor(int id) {
+            this.id = id;
+        }
+
+        protected void addTask(Task task) {
+            task.assignStart(ongoingTime);
+            tasks.add(task);
+            this.ongoingTime += task.getWeight();
+        }
+
+        protected int getOngoingTime() {
+            return this.ongoingTime;
+        }
+
+        protected int getId() {
+            return this.id;
+        }
+    }
+    private class Task {
+        private int weight;
+        private int start;
+        private String name;
+
+        protected Task(String name, int weight) {
+            this.weight = weight;
+            this.name = name;
+        }
+
+        protected Task(String name, int weight, int start) {
+            this.weight = weight;
+            this.name = name;
+            this.start = start;
+        }
+
+        protected void assignStart (int start) {
+            this.start = start;
+        }
+
+        public int getWeight() {
+            return this.weight;
+        }
+
+        protected int getStart() {
+            return this.start;
+        }
+
+    }
+}
