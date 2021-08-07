3d98b772625eb5896a89559c2299e172bb9d953e
henrym25 <henry3399@hotmail.co.nz>
2021-08-07 15:00:48 +1200

    [refactor]: Moved IO Handling to IO Handler Class
    Removed DotFileLoader and GraphHandler as they've become redundant

---
+++ src/main/java/softeng/project1/converters/IOHandler.java
@@ -0,0 +1,46 @@
+package softeng.project1.converters;
+
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+import java.io.IOException;
+
+import org.graphstream.stream.file.FileSourceDOT;
+
+
+
+public class IOHandler {
+    private final String GRAPH_FILE_PATH;
+    private Graph graph = new DefaultGraph("graph");
+
+
+    public IOHandler(String filePath) {
+        GRAPH_FILE_PATH = filePath;
+    }
+
+    /**
+     * Reads in DOT file from specified path and stores as Graph Object
+     */
+    public void readFile(){
+        FileSourceDOT fileSource = new FileSourceDOT();
+        fileSource.addSink(graph);
+
+        System.setProperty("org.graphstream.ui", "swing");
+        //Reads DOT file into graph object
+        try {
+            fileSource.readAll(GRAPH_FILE_PATH);
+        } catch( IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            fileSource.removeSink(graph);
+        }
+    }
+
+    /**
+     * Writes graphObject out as DOT File
+     */
+    public void writeFile(){
+
+    }
+
+
+}
