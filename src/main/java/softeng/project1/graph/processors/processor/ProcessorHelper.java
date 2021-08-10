1d9bf31210db4bb71e691e48943a08e9aadaca4e
Remus Courtenay <rcou199@aucklanduni.ac.nz>
2021-08-10 13:07:14 +1200

    [docs]: Javadoc for ProcessorHelper

--- src/main/java/softeng/project1/graph/processors/processor/ProcessorHelper.java
+++ src/main/java/softeng/project1/graph/processors/processor/ProcessorHelper.java
@@ -4,7 +4,7 @@ package softeng.project1.graph.processors.processor;
  * @author Remus Courtenay
  * @version 1.0
  * @since 1.8
- * Helper class, exists to try reduce duplicate code.
+ * Helper class, exists to try reduce duplicate code. Could be replaced by an abstract ProcessorState class.
  */
 class ProcessorHelper {
 
@@ -28,6 +28,15 @@ class ProcessorHelper {
         newProcessorSpaces[index][2] = taskID;
     }
 
+    /**
+     * Static helper method for inserting the final space that should always exist at the end of each processor
+     * schedule. Uses a unique ID to reduce the number of hashing collisions that could occur due to the hashing
+     * implementation.
+     *
+     * @param newProcessorSpaces : The set of spaces that exist between each task scheduled in a specific processor
+     *                             at a specific point in the algorithm.
+     * @param start : The location in the schedule to place the final space (Should be directly after the final task).
+     */
     static void addFinalSpace(int[][] newProcessorSpaces, int start) {
         // Unsure if this is faster than the method used above
         newProcessorSpaces[newProcessorSpaces.length-1] = new int[]{
