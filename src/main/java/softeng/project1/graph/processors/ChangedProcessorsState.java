9f3aeae6a56c9a272af369e09bb86fc164cc3205
Remus Courtenay <rcou199@aucklanduni.ac.nz>
2021-08-10 14:01:04 +1200

    [docs]: javadoc for ChangedProcessorsState

--- src/main/java/softeng/project1/graph/processors/ChangedProcessorsState.java
+++ src/main/java/softeng/project1/graph/processors/ChangedProcessorsState.java
@@ -5,22 +5,39 @@ import com.sangupta.murmur.Murmur3;
 import softeng.project1.graph.processors.processor.Processor;
 
 /**
- * ProcessorsState implements the Processors interface and is used to keep track of all the
- * different Processor objects.
+ * @author Remus Courtenay
+ * @version 1.0
+ * @since 1.8
+ *
+ * Implementation of ProcessorsState that specifically represents the state of a set of Processor objects where at least
+ * one has been changed.
  */
 public class ChangedProcessorsState extends ProcessorsState {
 
     private final int maxProcessorLength;
 
     /**
-     * ProcessorsState contructor which gets the Processor objects passed in as an array.
-     * @param processors
+     * Protected constructor for use only by the copyAndAddProcessor method from Processors.
+     * Direct instantiations should be used on OriginalProcessorsState objects only.
+     * @see OriginalProcessorsState
+     *
+     * @param processors : The set of Processor objects representing the current state of each processor.
+     * @param maxProcessorLength : The length of the longest stored Processor.
+     * @param idleTime : The sum total idle time of all stored Processor objects.
+     *
      */
     protected ChangedProcessorsState(Processor[] processors, int maxProcessorLength, int idleTime) {
         super(processors, idleTime);
         this.maxProcessorLength = maxProcessorLength;
     }
 
+    /**
+     * Generates a valid hash key int for use in a HashTable or HashMap
+     * based off of the state of each stored Processor.
+     * Overrides the base hashCode() Object method.
+     *
+     * @return : A valid hash key based off the Processor data.
+     */
     @Override
     public int hashCode() {
         
@@ -43,6 +60,15 @@ public class ChangedProcessorsState extends ProcessorsState {
 
     }
 
+    /**
+     * Deep equals method which ensures that equality is based off of
+     * Processor data/location instead of Object IDs.
+     * Overrides the base hashCode() Object method.
+     *
+     * @param otherObject : The other Processor set being equated to this one.
+     * @return : Whether or not each Processors object store the same values in the same order.
+     */
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
     @Override
     public boolean equals(Object otherObject) {
         Processors otherProcessors;
@@ -59,11 +85,23 @@ public class ChangedProcessorsState extends ProcessorsState {
         return true;
     }
 
+    /**
+     * Implementation of helper method from ProcessorsState.
+     *
+     * @param newProcessor : The changed Processor.
+     * @return : The length of the longest Processor schedule.
+     */
     @Override
     protected int calculateMaxProcessorLength(Processor newProcessor) {
         return Math.max(this.maxProcessorLength, newProcessor.getLength());
     }
 
+    /**
+     * Implementation of helper method from ProcessorsState.
+     *
+     * @param newProcessor : The changed Processor.
+     * @return : The sum total idle time of all stored Processor objects.
+     */
     @Override
     protected int calculateIdleTime(Processor newProcessor) {
         int lengthDiff = this.maxProcessorLength - newProcessor.getLength();
