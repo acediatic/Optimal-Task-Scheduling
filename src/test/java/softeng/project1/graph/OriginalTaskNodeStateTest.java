cb9b4f9e8d3bda48fcf23d0ff45f01049f8c9a7e
osamakashif <osamakashif1@hotmail.com>
2021-08-11 12:35:44 +1200

    [test]: isFree tests added
    Tests added to check isFree method works.

--- src/test/java/softeng/project1/graph/OriginalTaskNodeStateTest.java
+++ src/test/java/softeng/project1/graph/OriginalTaskNodeStateTest.java
@@ -1,6 +1,7 @@
 package softeng.project1.graph;
 
 import org.junit.jupiter.api.Test;
+import softeng.project1.graph.tasks.ChangedTaskNodeState;
 import softeng.project1.graph.tasks.OriginalTaskNodeState;
 import softeng.project1.graph.tasks.TaskNodeState;
 
@@ -21,6 +22,20 @@ public class OriginalTaskNodeStateTest {
         int[][] childLinks = new int[0][0];
         OriginalTaskNodeState tNode = new OriginalTaskNodeState(420,0, 0, childLinks, 0, 0);
         assertNotEquals(69, tNode.getTaskID());
+    }
+
+    @Test
+    void testIsFreeTrue() {
+        int[][] childLinks = new int[0][0];
+        OriginalTaskNodeState tNode = new OriginalTaskNodeState(150,0, 0, childLinks, 0, 0);
+        assertEquals(true, tNode.isFree());
+    }
+
+    @Test
+    void testIsFreeFalse() {
+        int[][] childLinks = new int[0][0];
+        OriginalTaskNodeState tNode = new OriginalTaskNodeState(150,0, 3, childLinks, 0, 5);
+        assertEquals(false, tNode.isFree());
     }
 
 }
