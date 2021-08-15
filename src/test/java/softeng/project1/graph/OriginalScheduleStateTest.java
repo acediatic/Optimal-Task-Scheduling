f64a2159b6df8fdaafdb0a8faf7a1afe9172dafa
osamakashif <osamakashif1@hotmail.com>
2021-08-16 00:48:43 +1200

    docs - JavaDoc
    JavaDoc header comment added.

--- src/test/java/softeng/project1/graph/OriginalScheduleStateTest.java
+++ src/test/java/softeng/project1/graph/OriginalScheduleStateTest.java
@@ -1,7 +1,9 @@
 package softeng.project1.graph;
 
 /**
- * TODO...
+ * This is the OriginalScheduleStateTest class for testing the OriginalScheduleState class.
+ * It uses JUnit Testing and thoroughly tests the different methods used in OriginalScheduleState.
+ * This includes inherited methods as well.
  * @author Osama Kashif
  */
 public class OriginalScheduleStateTest {
