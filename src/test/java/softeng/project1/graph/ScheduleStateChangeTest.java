794ec8933d3c64647f6a10e56a58cf8ee45fbe5a
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2021-08-16 11:30:04 +1200

    test - Tests setup.
    Test added for each function, need to implment them. For ScheduleStateChange.

--- src/test/java/softeng/project1/graph/ScheduleStateChangeTest.java
+++ src/test/java/softeng/project1/graph/ScheduleStateChangeTest.java
@@ -1,8 +1,15 @@
 package softeng.project1.graph;
 
+import org.junit.jupiter.api.Test;
+
 /**
- * TODO...
+ * This is the ScheduleStateChangeTest class for testing the ScheduleStateChange class.
+ * It uses JUnit Testing and thoroughly tests the different methods used in ScheduleStateChange.
  * @author Osama Kashif
  */
 public class ScheduleStateChangeTest {
+
+    @Test
+    void testRebuildSolutionPath() {}
+
 }
