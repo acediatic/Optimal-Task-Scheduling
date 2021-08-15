afe5daeb96cf306fdf4fab9670a815163b507a5e
Osama Kashif <63721736+osamakashif@users.noreply.github.com>
2021-08-16 11:35:13 +1200

    test - Tests setup.
    Tests added for each function, need to implment them. For ChangedScheduleState.

--- src/test/java/softeng/project1/graph/ChangedScheduleStateTest.java
+++ src/test/java/softeng/project1/graph/ChangedScheduleStateTest.java
@@ -1,8 +1,46 @@
 package softeng.project1.graph;
 
+import org.junit.jupiter.api.Test;
+
 /**
- * TODO...
+ * This is the ChangedScheduleStateTest class for testing the ChangedScheduleState class.
+ * It uses JUnit Testing and thoroughly tests the different methods used in ChangedScheduleState.
+ * This includes inherited methods as well.
  * @author Osama Kashif
  */
 public class ChangedScheduleStateTest {
+
+    @Test
+    void testDeepEquals() {}
+
+    @Test
+    void testGenerateStateChange() {}
+
+    @Test
+    void testGetOriginalSchedule() {}
+
+    @Test
+    void testGetTaskNode() {}
+
+    @Test
+    void testGetHashKey() {}
+
+    @Test
+    void testExpand() {}
+
+    @Test
+    void testFillProcessorPrerequisites() {}
+
+    @Test
+    void testGetMaxBottomLevel() {}
+
+    @Test
+    void testGetMaxDataReadyTime() {}
+
+    @Test
+    void testGetIdleTime() {}
+
+    @Test
+    void testToString() {}
+
 }
