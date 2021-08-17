a3ff2f2cd7543a65e96eeb42981d6985973f7807
osamakashif <osamakashif1@hotmail.com>
2021-08-17 15:26:56 +1200

    test - deepEquals
    Test for deepEquals implemented for OriginalScheduleState.

--- src/test/java/softeng/project1/graph/OriginalScheduleStateTest.java
+++ src/test/java/softeng/project1/graph/OriginalScheduleStateTest.java
@@ -1,6 +1,17 @@
 package softeng.project1.graph;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Test;
+import softeng.project1.graph.tasks.OriginalTaskNodeState;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * This is the OriginalScheduleStateTest class for testing the OriginalScheduleState class.
@@ -11,7 +22,25 @@ import org.junit.jupiter.api.Test;
 public class OriginalScheduleStateTest {
 
     @Test
-    void testDeepEquals() {}
+    void testDeepEquals() {
+        int[][] childLinks = new int[0][0];
+//        OriginalTaskNodeState tNode = new OriginalTaskNodeState(150,0, 0, childLinks, 0, 0);
+//        OriginalTaskNodeState fNode = new OriginalTaskNodeState(150,0, 0, childLinks, 0, 0);
+        Map<Integer, OriginalTaskNodeState>  tNode = new HashMap<>();
+        Map<Integer, OriginalTaskNodeState>  fNode = new HashMap<>();
+        Map<Integer, OriginalTaskNodeState>  otherFNode = new HashMap<>();
+        OriginalScheduleState o1 = new OriginalScheduleState(tNode, fNode, 5);
+        OriginalScheduleState o2 = new OriginalScheduleState(tNode, fNode, 5);
+        OriginalScheduleState o3 = new OriginalScheduleState(tNode, fNode, 0);
+        OriginalScheduleState o4 = new OriginalScheduleState(tNode, otherFNode, 5);
+        OriginalScheduleState o5 = new OriginalScheduleState(tNode, otherFNode, 5);
+        assertTrue(o1.deepEquals(o1));
+        assertFalse(o1.deepEquals(o2));
+        assertFalse(o1.deepEquals(o3));
+        assertFalse(o1.deepEquals(o4));
+        assertFalse(o1.deepEquals(o5));
+
+    }
 
     @Test
     void testGenerateStateChange() {}
