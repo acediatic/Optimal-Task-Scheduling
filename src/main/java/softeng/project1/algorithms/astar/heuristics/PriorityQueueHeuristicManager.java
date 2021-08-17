746b6127aeba00266ccd781cbf83b8d6d2fb5aa3
Adam <64721453+acediatic@users.noreply.github.com>
2021-08-17 14:34:31 +1200

    refactor: changed heuristic interface method names to be  more descriptive.

--- src/main/java/softeng/project1/algorithms/astar/heuristics/PriorityQueueHeuristicManager.java
+++ src/main/java/softeng/project1/algorithms/astar/heuristics/PriorityQueueHeuristicManager.java
@@ -8,28 +8,26 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueHeuristicManager implements HeuristicManager {
     private final PriorityQueue<AlgorithmStep> priorityQueue;
-    private final int taskLengthsSum;
-    private final short numberOfProcesses;
+    private final HeuristicHelper heuristicHelper;
 
     public PriorityQueueHeuristicManager(int taskLengthsSum, short numberOfProcesses) {
         this.priorityQueue = new PriorityQueue<>();
-        this.taskLengthsSum = taskLengthsSum;
-        this.numberOfProcesses = numberOfProcesses;
+        this.heuristicHelper = new HeuristicHelper(taskLengthsSum, numberOfProcesses);
     }
 
     @Override
     // TODO null check?
-    public AlgorithmStep get() {
+    public AlgorithmStep getAlgorithmStepQueueHead() {
         return priorityQueue.poll();
     }
 
     @Override
-    public void addAll(List<Schedule> newFringeSchedules) {
+    public void addAllSchedules(List<Schedule> newFringeSchedules) {
         List<AlgorithmStep> algoSteps = new ArrayList<>(newFringeSchedules.size());
 
         // Convert schedules to algorithm steps
         for (Schedule schedule : newFringeSchedules) {
-            algoSteps.add(new AlgorithmStep(calculateHeuristicValue(schedule), schedule));
+            algoSteps.add(heuristicHelper.getAlgoStep(schedule));
         }
 
         // add all to priority queue.
@@ -37,23 +35,12 @@ public class PriorityQueueHeuristicManager implements HeuristicManager {
     }
 
     @Override
-    public void add(Schedule fringeSchedule) {
+    public void addSchedule(Schedule fringeSchedule) {
         // TODO pruning, check if should be added
-
         // should be added, so calculate heuristic, wrap in algorithm step and add.
-        AlgorithmStep algoStep = new AlgorithmStep(calculateHeuristicValue(fringeSchedule), fringeSchedule);
-        priorityQueue.add(algoStep);
+        priorityQueue.add(heuristicHelper.getAlgoStep(fringeSchedule));
     }
 
-    // TODO describe heuristic
-    /*
-     * f(s) = max{f_idle-time(s), f_bl(s), f_DRT(s)}
-     */
-    private int calculateHeuristicValue(Schedule schedule) {
-        // Two Math.max calls because it only takes two inputs, and we need three
-        return Math.max(Math.max(
-                        (taskLengthsSum + schedule.getIdleTime()) / numberOfProcesses,      // f_idle_time
-                        schedule.getMaxBottomLevel()),                                      // f_bl
-                schedule.getMaxDataReadyTime());                                            // f_DRT
-    }
+
+
 }
