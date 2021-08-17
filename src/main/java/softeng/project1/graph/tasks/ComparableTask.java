fa910377cb077a6c27b8e70deca4ad1e84d43f25
Adam Sinclair <hello@adamsinclair.kiwi>
2021-08-18 10:44:55 +1200

    [feat]: changed ComparisonTask to also compare edge weights.
    HashMaps are used to create pairings between a node and a
    weight. The map.equals() method is used to confirm the two
    hash maps are the same, indicating same ingoing and outgoing
    edges and weights. This, along with the task weights, confirms
    equivalent children.

--- src/main/java/softeng/project1/graph/tasks/ComparableTask.java
+++ src/main/java/softeng/project1/graph/tasks/ComparableTask.java
@@ -1,11 +1,11 @@
 package softeng.project1.graph.tasks;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 
@@ -17,25 +17,32 @@ public class ComparableTask implements Comparable<ComparableTask> {
     private static final String PROCESSING_COST_ATTRIBUTE_KEY = "Weight";
     private final Node task;
     private final int taskWeight;
-    private final int parentSetHash;
-    private final int childSetHash;
-    private final Collection<? extends Node> parents;
-    private final Collection<? extends Node> children;
+    private final HashMap<Node, Integer> parentEdgeHashMap;
+    private final HashMap<Node, Integer> childEdgeHashMap;
 
     // TODO confirm the edge object is the same, and so can be used for equality comparison.
     public ComparableTask(Node task, int taskWeight) {
         this.task = task;
         this.taskWeight = taskWeight;
 
-        this.parents = (Collection<? extends Node>) task.enteringEdges().collect(Collectors.toCollection(ArrayList::new));
-        HashSet<Node> parentSet = new HashSet<>(parents.size());
-        parentSet.addAll(parents);
-        this.parentSetHash = parentSet.hashCode();
+        ArrayList<Edge> parents = task.enteringEdges().collect(Collectors.toCollection(ArrayList::new));
+        this.parentEdgeHashMap = new HashMap<>();
 
-        this.children = (Collection<? extends Node>) task.leavingEdges().collect(Collectors.toCollection(ArrayList::new));
-        HashSet<Node> childSet = new HashSet<>(children.size());
-        childSet.addAll(children);
-        this.childSetHash = childSet.hashCode();
+        for (Edge parentEdge : parents) {
+            Node parentNode = parentEdge.getSourceNode();
+            int weight = 0;//(int) parentEdge.getAttribute(PROCESSING_COST_ATTRIBUTE_KEY);
+            parentEdgeHashMap.put(parentNode, weight);
+        }
+
+
+        ArrayList<Edge> children = task.leavingEdges().collect(Collectors.toCollection(ArrayList::new));
+        this.childEdgeHashMap = new HashMap<>();
+
+        for (Edge childEdge : children) {
+            Node childNode = childEdge.getTargetNode();
+            int weight = 0;//(int) childEdge.getAttribute(PROCESSING_COST_ATTRIBUTE_KEY);
+            childEdgeHashMap.put(childNode, weight);
+        }
     }
 
     public Node getTask() {
@@ -47,26 +54,11 @@ public class ComparableTask implements Comparable<ComparableTask> {
     public int compareTo(@NotNull ComparableTask o) {
         if (this.taskWeight != o.taskWeight) {
             return this.taskWeight - o.taskWeight;
-        } else if (this.childSetHash != o.childSetHash) {
-            return this.childSetHash - o.childSetHash;
-        } else if (this.parentSetHash != o.parentSetHash) {
-            return this.parentSetHash - o.parentSetHash;
+        } else if (!this.parentEdgeHashMap.equals(o.parentEdgeHashMap)) {
+            return this.parentEdgeHashMap.hashCode() - o.parentEdgeHashMap.hashCode();
+        } else if (this.childEdgeHashMap.equals(o.parentEdgeHashMap)) {
+            return this.childEdgeHashMap.hashCode() - o.parentEdgeHashMap.hashCode();
         } else {
-            // check communication weights
-            ArrayList<Collection<? extends Node>> parentsAndChildren = new ArrayList<>();
-            parentsAndChildren.add(this.children);
-            parentsAndChildren.add(this.parents);
-
-            for (Collection<? extends Node> edgeCollection : parentsAndChildren) {
-                for (Node edgeNode : edgeCollection) {
-                    double thisWeight = (double) this.task.getEdgeBetween(edgeNode).getAttribute(PROCESSING_COST_ATTRIBUTE_KEY);
-                    double oWeight = (double) o.task.getEdgeBetween(edgeNode).getAttribute(PROCESSING_COST_ATTRIBUTE_KEY);
-
-                    if (thisWeight - oWeight != 0) {
-                        return (int) (thisWeight - oWeight);
-                    }
-                }
-            }
             // Equivalent children.
             return 0;
         }
