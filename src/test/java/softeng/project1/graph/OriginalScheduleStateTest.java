2bde0d8a090f2dd774e793058a175f26a2eaa56c
osamakashif <osamakashif1@hotmail.com>
2021-08-18 23:59:40 +1200

    test - TaskID test added; refactor - Refactored to improve testing.

--- src/test/java/softeng/project1/graph/OriginalScheduleStateTest.java
+++ src/test/java/softeng/project1/graph/OriginalScheduleStateTest.java
@@ -1,13 +1,15 @@
 package softeng.project1.graph;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import softeng.project1.graph.tasks.ChangedTaskNodeState;
+import softeng.project1.graph.tasks.OriginalTaskNodeState;
 import softeng.project1.graph.tasks.TaskNode;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is the OriginalScheduleStateTest class for testing the OriginalScheduleState class.
@@ -17,16 +19,33 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class OriginalScheduleStateTest {
 
+    private static int[][] childLinks = new int[2][2];
+    private static OriginalTaskNodeState original1 = new OriginalTaskNodeState(150,20, 3, childLinks, 0, 0);
+    private static OriginalTaskNodeState original2 = new OriginalTaskNodeState(150,20, 3, childLinks, 0, 0);
+    private static Map<Integer, TaskNode>  tNode = new HashMap<>();
+    private static Map<Integer, TaskNode>  fNode = new HashMap<>();
+    private static Map<Integer, TaskNode>  otherFNode = new HashMap<>();
+    private static OriginalScheduleState o1;
+    private static OriginalScheduleState o2;
+    private static OriginalScheduleState o3;
+    private static OriginalScheduleState o4;
+    private static OriginalScheduleState o5;
+
+    @BeforeAll
+    private static void setup() {
+        tNode.put(0, original1);
+        tNode.put(1, original2);
+        fNode.put(0, original1);
+        fNode.put(1, original2);
+        o1 = new OriginalScheduleState(tNode, fNode, 5);
+        o2 = new OriginalScheduleState(tNode, fNode, 5);
+        o3 = new OriginalScheduleState(tNode, fNode, 0);
+        o4 = new OriginalScheduleState(tNode, otherFNode, 5);
+        o5 = new OriginalScheduleState(tNode, otherFNode, 5);
+    }
+
     @Test
-    void testDeepEquals() {
-        Map<Integer, TaskNode>  tNode = new HashMap<>();
-        Map<Integer, TaskNode>  fNode = new HashMap<>();
-        Map<Integer, TaskNode>  otherFNode = new HashMap<>();
-        OriginalScheduleState o1 = new OriginalScheduleState(tNode, fNode, 5);
-        OriginalScheduleState o2 = new OriginalScheduleState(tNode, fNode, 5);
-        OriginalScheduleState o3 = new OriginalScheduleState(tNode, fNode, 0);
-        OriginalScheduleState o4 = new OriginalScheduleState(tNode, otherFNode, 5);
-        OriginalScheduleState o5 = new OriginalScheduleState(tNode, otherFNode, 5);
+    public void testDeepEquals() {
         assertTrue(o1.deepEquals(o1));
         assertFalse(o1.deepEquals(o2));
         assertFalse(o1.deepEquals(o3));
@@ -36,33 +55,37 @@ public class OriginalScheduleStateTest {
     }
 
     @Test
-    void testGenerateStateChange() {}
+    public void testGenerateStateChange() {}
 
     @Test
-    void testGetOriginalSchedule() {}
+    public void testGetOriginalSchedule() {}
 
     @Test
-    void testGetTaskNode() {}
+    public void testGetTaskNode() {
+        assertEquals(original1,o1.getTaskNode(0));
+        assertNotEquals(original2,o1.getTaskNode(0));
+        assertEquals(o1.getTaskNode(0),o2.getTaskNode(0));
+    }
 
     @Test
-    void testGetHashKey() {}
+    public void testGetHashKey() {}
 
     @Test
-    void testExpand() {}
+    public void testExpand() {}
 
     @Test
-    void testFillProcessorPrerequisites() {}
+    public void testFillProcessorPrerequisites() {}
 
     @Test
-    void testGetMaxBottomLevel() {}
+    public void testGetMaxBottomLevel() {}
 
     @Test
-    void testGetMaxDataReadyTime() {}
+    public void testGetMaxDataReadyTime() {}
 
     @Test
-    void testGetIdleTime() {}
+    public void testGetIdleTime() {}
 
     @Test
-    void testToString() {}
+    public void testToString() {}
 
 }
