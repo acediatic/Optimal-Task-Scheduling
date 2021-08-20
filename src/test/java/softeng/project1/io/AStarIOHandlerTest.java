7143c23704115b8a42f9a08791b1b6b20ed2f630
Syed Kazmi <skaz269@aucklanduni.ac.nz>
2021-08-20 13:03:06 +1200

    [feat] Initial testing for AStarIOHandler class

---
+++ src/test/java/softeng/project1/io/AStarIOHandlerTest.java
@@ -0,0 +1,32 @@
+package softeng.project1.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * This is the test class for the AStarIOHandler Class
+ * It utilizes JUNIT 5 to test the different methods found within the class
+ * @author Syed Kazmi
+ */
+class AStarIOHandlerTest {
+
+    @Test
+    void readFile() {
+    }
+
+    @Test
+    void writeFile() {
+    }
+
+    @Test
+    void getSumWeights() {
+    }
+
+    @Test
+    void getTaskName() {
+    }
+
+    @Test
+    void getGraph() {
+    }
+}
