d60a24b28a66ce5b17e98ccc3659bc08274e7a10
osamakashif <osamakashif1@hotmail.com>
2021-08-21 22:36:50 +1200

    feat - Updated testing shell.
    Updated testing shell for various permutations.

--- testAllGraphs.sh
+++ testAllGraphs.sh
@@ -2,4 +2,21 @@ for FILE in src/test/java/softeng/project1/full_graph_tests/inputGraphs/*; do
   echo "*** SEQUENTIAL FOR $FILE ***"
   java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2;
   java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2 -v;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4 -v;
+  echo "*** PARALLEL WITH -P 2 FOR $FILE ***"
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2 -p 2;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4 -p 2;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2 -p 2 -v;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4 -p 2 -v;
+  echo "*** PARALLEL WITH -P 4 FOR $FILE ***"
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2 -p 4;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4 -p 4;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2 -p 4 -v;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4 -p 4 -v;
+  echo "*** PARALLEL WITH -P 8 FOR $FILE ***"
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2 -p 8;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4 -p 8;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 2 -p 8 -v;
+  java -Xmx4G -jar ./target/scheduler-0.0-jar-with-dependencies.jar "$FILE" 4 -p 8 -v;
 done
