ff7e6c6e2acacd8e1d1995988263992247c71f7e
Remus Courtenay <remuscourtenay@gmail.com>
2021-08-22 21:07:17 +1200

    [docs]: Filled in comments in GuiData and AlgorithmDataCache

--- src/main/java/softeng/project1/gui/AlgorithmDataCache.java
+++ src/main/java/softeng/project1/gui/AlgorithmDataCache.java
@@ -7,25 +7,30 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
- * A class to store cached data from the algorithm, so that the GUI does not have to interrupt
- * the scheduling algorithm as much. It polls for new data at a fixed rate (100ms).
+ * @author Remus Courtenay
+ * @version 1.0
+ * @since 1.8
+ * Middle man service that allows for asynchronous data accessing from the GUI to the scheduling algorithm.
+ * New data is requested from the algorithm every 100 milliseconds, although retrieval of that data can take varied
+ * amounts of time depending on the algorithm implementation.
+ *
+ * The freshest available data is stored in the cache and can be accessed by the GUI as needed.
  */
 public class AlgorithmDataCache {
 
-    private final SchedulingAlgorithm algorithm; // the algorithm performing the scheduling
-    private final ScheduledExecutorService scheduledExecutor; // the executor service for the algorithm
+    private static final int INITIAL_DATA_ACCESS_DELAY = 0;
+    private static final int DATA_ACCESS_REQUEST_PERIOD = 100;
+
     private GuiData cachedData; // the cached data to use.
 
     public AlgorithmDataCache(SchedulingAlgorithm algorithm) {
-
-        this.algorithm = algorithm;
-        this.scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+        ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
         scheduledExecutor.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
                 cachedData = algorithm.getGuiData();
             }
-        }, 100, 100, TimeUnit.MILLISECONDS);
+        }, INITIAL_DATA_ACCESS_DELAY, DATA_ACCESS_REQUEST_PERIOD, TimeUnit.MILLISECONDS);
     }
 
     /**
--- src/main/java/softeng/project1/gui/GuiData.java
+++ src/main/java/softeng/project1/gui/GuiData.java
@@ -6,17 +6,21 @@ import softeng.project1.algorithms.astar.heuristics.AlgorithmStep;
 import java.util.List;
 
 /**
- * A class to store data used by the GUI to display the visualisation
+ * @author Remus Courtenay
+ * @version 1.0
+ * @since 1.8
+ * POJO that stores data read from the algorithm for use in the GUI.
  */
 public class GuiData {
 
     private final List<int[]> currentBest;  // the current best schedule (partial or optimal)
-    private final int currentHeuristic;     // the heuristc value of the corresponding schedule
+    private final int currentHeuristic;     // the heuristic value of the corresponding schedule
     private final AlgorithmState state;     // the AlgorithmState for the corresponding schedule
     private final int numSchedulesChecked;  // the approximate number of states checked so far.
 
     public GuiData(AlgorithmStep step, AlgorithmState state, int numSchedulesChecked) {
 
+        // Step can be null if the algorithm hasn't started so that's handled here
         if (step == null) {
             this.currentBest = null;
             this.currentHeuristic = 0;
@@ -30,6 +34,7 @@ public class GuiData {
 
     }
 
+    // ------------------ Standard Getter Methods ------------------
 
     public List<int[]> getCurrentBestScheduleArray() {
         return this.currentBest;
